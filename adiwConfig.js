b62f18f2a25b78e468f03e03fddccf97f9c71b6b
PINO MODDER <120091085+pinomodder@users.noreply.github.com>
2025-03-21 01:08:50 +0700

    PINO MODZ
    ϟ𝐏𝐈𝐍𝐎 𝐌𝐎𝐃𝐙〽

---
+++ adiwConfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'approval': {
+    'num': '6288226852469',
+    'text': "setujui",
+    'set': "👀 Harus mendapatkan persetujuan oleh creator script, Jika ingin memakai script ini",
+    'greet': "*Disetujui Oleh Creator, Silahkan Restart Panel Atau Run Ulang script* ☠️ "
+  },
+  'creatorScript': '6288226852469',
+  'filePath': "./Security/approval",
+  'checkFilePath': "./recodeteam.js",
+  'codeToDetect': 'PinoRecodeTeam();'
+};
